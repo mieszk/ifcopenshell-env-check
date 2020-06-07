2de64ca96e9755d9da7dbb3ecc57b16240762fe0
Mieszko Boczkowski <mieszko.boczkowski@gmail.com>
2020-06-07 23:24:26 +0200

    use actual docker

--- build_models.py
+++ build_models.py
@@ -7,7 +7,7 @@ env_names = [file.name for file in sorted(Path("environments").iterdir())]
 for env in env_names:
     print(f"Running env {env}")
     subprocess.call([
-        "podman", "run",
+        "docker", "run",
         "-v", "./ifc-files:/var/ifc-files:Z",
         "-v", "./models:/var/models:Z",
         f"ifc-env-check:{env}"
